a66aa0b120aeb382758b5067d1a3deb0c843a045
Pedro H. Braz Vallocci <50343231+pbrazval@users.noreply.github.com>
2024-06-19 20:05:00 -0400

    oop nlp

--- src/main.py
+++ src/main.py
@@ -5,11 +5,12 @@ yearrange = range(2006,2023)
 download_10ks_again = False
 
 if __name__ == '__main__':
-   #dl.The10KDownloader(yearrange = yearrange, redo = download_10ks_again)
-   #dl.The1AConverter(yearrange = yearrange, redo = download_10ks_again)
-    #ngr = nlp.NGrammer(yearrange=yearrange, from_pickle=True) 
-    print("NGrammer initialized")
-    # dic = nlp.Dictionary(ngr, dicpath = 'output/dictionary.txt', from_pickle=False) # Need to test
+    if False:
+        dl.The10KDownloader(yearrange = yearrange, redo = download_10ks_again)
+        dl.The1AConverter(yearrange = yearrange, redo = download_10ks_again)
+        ngr = nlp.NGrammer(yearrange=yearrange, from_pickle=True) 
+        print("NGrammer initialized")
+        dic = nlp.Dictionary(ngr, dicpath = 'output/dictionary.txt', from_pickle=False) # Need to test
     bow = nlp.BagOfWords(dicname = "dicfullmc10thr10defnob40noa0_8", from_pickle = True)
     lda = nlp.LDA(bow, None, modelname = "dicfullmc10thr10defnob40noa0_8_10_t", redo = False)
     # lda.visualize_topics()
